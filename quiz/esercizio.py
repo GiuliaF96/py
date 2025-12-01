ff31b12bf5a8f6349be4b466d5f0d620136cbafd
Giulia <giuliaferrandina@gmail.com>
2025-12-01 11:41:19 +0100

    quiz commit

---
+++ quiz/esercizio.py
@@ -0,0 +1,40 @@
+"""
+def valida_scelta(scelta : str) -> bool:
+
+   if scelta == "A":
+        return True
+   else:
+        return False
+
+
+
+
+def mostra_domanda() -> None:
+    print(
+        
+        
+
+        Chi parteciperà a Sanremo 2026?
+
+        A. Nayt
+        B. La Nina
+        C. J-AX
+        D. Rocco Papaleo
+
+        
+
+    )
+
+    mostra_domanda()
+
+
+    def raccogli_risposta() -> str:
+        risposta: str = input("Inserisci la tua scelta: ")
+        return risposta
+    
+    #mostra_domanda ()
+
+    print(raccogli_risposta())
+
+    
+"""
