88724f215f2978d79878465c61d28183a8cc5ce3
Giulia <giuliaferrandina@gmail.com>
2025-12-03 11:31:23 +0100

    liste

---
+++ quiz/strutture-dati.py
@@ -0,0 +1,36 @@
+#liste
+"""stringhe: list[str] = ["Pippo"]
+
+stringhe.append("Pluto")
+
+for x in stringhe:
+
+    print(x)
+
+stringhe.append("Minnie")
+stringhe.append("Paperino")
+
+value_to_check_and_delete: str = "Pluto"
+
+deleted_values: list[str] = []
+
+is_value_in_the_list : bool = value_to_check_and_delete in stringhe
+
+if is_value_in_the_list == True:
+
+    index_value_to_delete = stringhe.index(value_to_check_and_delete.capitalize())
+    deleted_value = stringhe.pop(index_value_to_delete)
+
+    deleted_values.append(deleted_value)
+
+else: 
+    print(f"{value_to_check_and_delete}Non esiste nella lista  {stringhe}")
+
+print("*"*30)
+print(stringhe)
+print(deleted_values)
+"""
+
+#dizionari 
+
+dizionario: dict[str, int] = {
